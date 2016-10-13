41d5ed096592ffb55558b3d1f4291cec5cb6919c
zwei-und-vierzig <arshava.k@yandex.ru>
2016-10-13 22:25:29 +0300

    Update settings.py

--- almasta_tables/settings.py
+++ almasta_tables/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+p6089zxru64-35wk#l3bwr-117&yzh3+w2l&ni9a$kzy7sa(='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
